e846288aaaabe91677b4adf47b38cde062286427
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-05-02 22:31:47 +0900

    feat: send tokyo weather data from json

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -10,16 +11,21 @@ bucket = "test"
 client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
-p = Point("my_measurement").tag(
-    "location", "Prague").field("temperature", 25.3)
-write_api.write(bucket=bucket, record=p)
+
+f = open('test-weather-data.json', 'r')
+weather = json.load(f)
+weather_hourly = weather["hourly"]
+for k in weather_hourly:
+    data = f'weather,location=Tokyo temperature={k["temp"] - 273.15} {k["dt"]}000000000'
+    print(data)
+    write_api.write(bucket, org, data)
 
 query_api = client.query_api()
 query = '''from(bucket: "test")
-  |> range(start: -24h)
-  |> filter(fn: (r) => r["_measurement"] == "mem")
-  |> filter(fn: (r) => r["_field"] == "used_percent")
-  |> filter(fn: (r) => r["host"] == "host1")'''
+  |> range(start: -48h)
+  |> filter(fn: (r) => r["_measurement"] == "weather")
+  |> filter(fn: (r) => r["_field"] == "temperature")
+  |> filter(fn: (r) => r["location"] == "Tokyo")'''
 print(query)  # debug
 result = query_api.query(query)
 for table in result:
